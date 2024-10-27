c0c64423f1065343d4666e849874a59dcc6af22d
ztx <30760655@qq.com>
2024-10-27 17:47:13 +0800

    2

--- AX6-2/diy.sh
+++ AX6-2/diy.sh
@@ -8,40 +8,30 @@ function git_sparse_clone() {
   mv -f $@ ../package
   cd .. && rm -rf $repodir
 }
-# Default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 
 # Add packages
 #添加科学上网源
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-git clone -depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
-#git clone --depth=1 https://github.com/sirpdboy/luci-app-lucky package/lucky
-#git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
-#git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
-
+git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
 git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
-#git clone --depth=1 https://github.com/0x676e67/luci-theme-design package/luci-theme-design
-#git clone --depth=1 https://github.com/0x676e67/luci-app-design-config package/luci-app-design-config
-#git clone --depth 1 https://github.com/sbwml/luci-app-alist package/alist
-
-#添加自定义的软件包源
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-theme-design
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-design-config
-
+git clone --depth 1 https://github.com/sbwml/luci-app-alist package/alist
+#git clone --depth=1  https://github.com/kenzok8/small-package package/small-package
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-zerotier
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages vlmcsd
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-vlmcsd
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-socat
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-openvpn-server
 
 # Remove packages
 #删除lean库中的插件，使用自定义源中的包。
-#rm -rf feeds/packages/net/v2ray-geodata
-#rm -rf feeds/luci/themes/luci-theme-argon
-#rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/packages/net/alist
+rm -rf feeds/luci/applications/luci-app-alist
 #rm -rf feeds/packages/net/mosdns
-#rm -rf feeds/packages/utils/v2dat
 #rm -rf feeds/luci/applications/luci-app-mosdns
-#rm -rf feeds/luci/themes/luci-theme-design
-#rm -rf feeds/luci/applications/luci-app-design-config
+
 
 # Default IP
 #sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
