dd8633bee102e4a44fbfa92b7f788925624365a7
ztx <30760655@qq.com>
2024-10-27 09:47:27 +0800

    update

---
+++ AX6-2/diy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+# Default IP
+sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+
+# Add packages
+#添加科学上网源
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+git clone -depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
+#git clone --depth=1 https://github.com/sirpdboy/luci-app-lucky package/lucky
+#git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
+#git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
+
+git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
+#git clone --depth=1 https://github.com/0x676e67/luci-theme-design package/luci-theme-design
+#git clone --depth=1 https://github.com/0x676e67/luci-app-design-config package/luci-app-design-config
+#git clone --depth 1 https://github.com/sbwml/luci-app-alist package/alist
+
+#添加自定义的软件包源
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-theme-design
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-design-config
+
+
+# Remove packages
+#删除lean库中的插件，使用自定义源中的包。
+#rm -rf feeds/packages/net/v2ray-geodata
+#rm -rf feeds/luci/themes/luci-theme-argon
+#rm -rf feeds/luci/applications/luci-app-argon-config
+#rm -rf feeds/packages/net/mosdns
+#rm -rf feeds/packages/utils/v2dat
+#rm -rf feeds/luci/applications/luci-app-mosdns
+#rm -rf feeds/luci/themes/luci-theme-design
+#rm -rf feeds/luci/applications/luci-app-design-config
+
+# Default IP
+#sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
