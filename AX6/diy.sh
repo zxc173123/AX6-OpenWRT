abb1ca3dac8d81e038d5430469791e60ddad09fd
ztx <30760655@qq.com>
2024-10-28 10:19:50 +0800

    fine

--- AX6/diy.sh
+++ AX6/diy.sh
@@ -16,14 +16,13 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwr
 git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
+git clone -b v5-lua --single-branch --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
+git clone -b v3.35.0 --single-branch --depth 1 https://github.com/sbwml/luci-app-alist package/alist
 #git clone --depth=1 https://github.com/sirpdboy/luci-app-lucky package/lucky
 #git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
 #git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
-
-#git clone -b v5-lua --single-branch --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
 #git clone --depth=1 https://github.com/0x676e67/luci-theme-design package/luci-theme-design
 #git clone --depth=1 https://github.com/0x676e67/luci-app-design-config package/luci-app-design-config
-git clone -b v3.35.0 --single-branch --depth 1 https://github.com/sbwml/luci-app-alist package/alist
 
 #添加自定义的软件包源
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-theme-design
@@ -36,8 +35,8 @@ rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
-#rm -rf feeds/packages/net/mosdns
-#rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/luci-app-mosdns
 #rm -rf feeds/luci/themes/luci-theme-design
 #rm -rf feeds/luci/applications/luci-app-design-config
 
