ebb0b6df80959740a7d8fcea5437222ecbe82fab
ztx <30760655@qq.com>
2024-11-03 13:37:21 +0800

    update

--- AX6-2/diy.sh
+++ AX6-2/diy.sh
@@ -11,24 +11,24 @@ function git_sparse_clone() {
 
 # Add packages
 #添加科学上网源
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
-git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
-git clone --depth 1 https://github.com/sbwml/luci-app-alist package/alist
+#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
+#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+#git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+#git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+#git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
+#git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
+#git clone --depth 1 https://github.com/sbwml/luci-app-alist package/alist
 #git clone --depth=1  https://github.com/kenzok8/small-package package/small-package
-git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-zerotier
-git_sparse_clone main https://github.com/kiddin9/kwrt-packages vlmcsd
-git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-vlmcsd
-git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-socat
-git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-openvpn-server
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-zerotier
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages vlmcsd
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-vlmcsd
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-socat
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-openvpn-server
 
 # Remove packages
 #删除lean库中的插件，使用自定义源中的包。
-rm -rf feeds/packages/net/alist
-rm -rf feeds/luci/applications/luci-app-alist
+#rm -rf feeds/packages/net/alist
+#rm -rf feeds/luci/applications/luci-app-alist
 #rm -rf feeds/packages/net/mosdns
 #rm -rf feeds/luci/applications/luci-app-mosdns
 
