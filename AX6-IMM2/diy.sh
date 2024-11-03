7d9aecf1dde832fb7d8468878e28b71579eafe42
ztx <30760655@qq.com>
2024-11-03 19:39:00 +0800

    update

---
+++ AX6-IMM2/diy.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# Add packages
+#添加科学上网源
+#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
+#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+#git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+#git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+#git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
+#git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
+#git clone --depth 1 https://github.com/sbwml/luci-app-alist package/alist
+#git clone --depth=1  https://github.com/kenzok8/small-package package/small-package
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-zerotier
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages vlmcsd
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-vlmcsd
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-socat
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-openvpn-server
+git clone -b js https://github.com/papagaye744/luci-theme-design package/luci-theme-design
+
+#替换luci-app-socat为https://github.com/chenmozhijin/luci-app-socat
+rm -rf feeds/luci/applications/luci-app-socat
+git_sparse_clone main https://github.com/chenmozhijin/luci-app-socat luci-app-socat
+
+# Remove packages
+#删除lean库中的插件，使用自定义源中的包。
+#rm -rf feeds/packages/net/alist
+#rm -rf feeds/luci/applications/luci-app-alist
+#rm -rf feeds/packages/net/mosdns
+#rm -rf feeds/luci/applications/luci-app-mosdns
+
+
+
+
+
+#修改默认IP
+#sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+
+#修改主机名
+sed -i "s/hostname='ImmortalWrt'/hostname='Redmi-AX6'/g" package/base-files/files/bin/config_generate
