9fd6718c2befa68f3186a8937a3897024a29c4f0
ztx <30760655@qq.com>
2024-11-09 16:59:20 +0800

    update imm congig

--- AX6-IMM/diy.sh
+++ AX6-IMM/diy.sh
@@ -23,12 +23,16 @@ function git_sparse_clone() {
 #git_sparse_clone main https://github.com/kiddin9/kwrt-packages vlmcsd
 #git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-vlmcsd
 #git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-socat
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-openvpn-server
-git clone -b js https://github.com/papagaye744/luci-theme-design package/luci-theme-design
 
-# 替换luci-app-openvpn-server
+# 替换luci-app-openvpn-server imm源的启动不了服务！
 rm -rf feeds/luci/applications/luci-app-openvpn-server
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-openvpn-server
+# 调整 openvpn-server 到 VPN 菜单
+sed -i 's/services/vpn/g' package/luci-app-openvpn-server/luasrc/controller/*.lua
+sed -i 's/services/vpn/g' package/luci-app-openvpn-server/luasrc/model/cbi/openvpn-server/*.lua
+sed -i 's/services/vpn/g' package/luci-app-openvpn-server/luasrc/view/openvpn/*.htm
+
+# git clone -b js https://github.com/papagaye744/luci-theme-design package/luci-theme-design
 
 #替换luci-app-socat为https://github.com/chenmozhijin/luci-app-socat
 rm -rf feeds/luci/applications/luci-app-socat
